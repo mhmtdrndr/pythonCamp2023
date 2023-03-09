2b9e4b43354eccf6eff73fc53cf8bddc59a427c2
Mehmet Derindere <83140766+mhmtdrndr@users.noreply.github.com>
2023-03-09 16:17:42 +0300

    Add files via upload

---
+++ odev_01/main.py
@@ -0,0 +1,11 @@
+kursIsmi = "(2023) Yazılım Geliştirici Yetiştirme Kampı - Python & Selenium"
+
+kategori = ["Programlama"]
+egitmen = ["Engin Demiroğ", "Halit Enes Kalaycı"]
+
+giriYaptiMi = False
+
+if girisYaptiMi:
+    print("Sisteme başarılı bir şekilde giriş yapıldı")
+else:
+    print("kullanıcı adı ve şifrenizle giriş yapabilirsiniz.")
