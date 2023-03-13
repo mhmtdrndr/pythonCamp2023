0e98601adb8f19ac25071681b4e733dd307627e1
Mehmet Derindere <83140766+mhmtdrndr@users.noreply.github.com>
2023-03-13 11:19:16 +0300

    Update odev02.py
    değişiklik yapıldı

--- odev02/odev02.py
+++ odev02/odev02.py
@@ -1,19 +1,35 @@
-studentList = []
+studentList = ["Lionel Messi", "Cristiano Ronaldo", "Mauro Icardi"]
 
 def fullName(name, surname):
     nameSurnameValue = (name + " " + surname)
     return nameSurnameValue
 
+
+# Aldığı isim soy isim ile listeye öğrenci ekleyen
 def addList(name, surname):
     studentList.append(fullName(name, surname))
 
+
+# Aldığı isim soy isim ile eşleşen değeri listeden kaldıran
+def deleteStudent(student):
+    if student in studentList:
+        studentList.remove(student)
+        print(f"{student} isimli öğrenci listeden silindi")
+    else:
+        print(f"{student} isimli öğrenci listede yok")
+
+
+#Listeye birden fazla öğrenci eklemeyi mümkün kılan
+
+
+
+# Listedeki tüm öğrencileri tek tek ekrana yazdıran
 def printList(list):
     for value in list:
         print(value)
 
-name = input("Öğrenci ismini giriniz : ")
-surName = input("Öğrenci soyismini giriniz :")
 
-addList(name, surName)
+# Öğrencinin listedeki index numarası öğrenci numarası olarak kabul edildiğini düşünerek öğrencinin numarasını öğrenmeyi mümkün kılan
 
-printList(studentList)
+
+# Listeden birden fazla öğrenci silmeyi mümkün kılan (döngü kullanınız)
